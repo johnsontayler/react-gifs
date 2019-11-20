a17ce99985abdde36b380863134688cadaf7986f
Tayler Johnson <johnsontayler@me.com>
2019-11-20 19:05:44 -0300

    gif component

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    return (
+      <img src="https://media.giphy.com/media/3og0IHYSntVrxUo9kA/giphy.gif" alt="gif" className="selected-gif" />
+    );
+  }
+}
+
+export default Gif;
